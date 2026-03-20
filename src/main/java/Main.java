d5b6ac0d54300f0ff6f0d4db5f27ceb43e6ec741
Rags <amstein4920@gmail.com>
2026-03-20 23:42:41 +0800

    Added Concurrent Client Connections

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,47 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) {
-        ServerSocket serverSocket = null;
-        Socket clientSocket = null;
         int port = 6379;
-        try {
-            serverSocket = new ServerSocket(port);
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
             // Since the tester restarts your program quite often, setting SO_REUSEADDR
             // ensures that we don't run into 'Address already in use' errors
             serverSocket.setReuseAddress(true);
             // Wait for connection from client.
-            clientSocket = serverSocket.accept();
-        } catch (IOException e) {
-            System.out.println("IOException: " + e.getMessage());
-        } finally {
-            try {
-                if (clientSocket != null) {
-                    clientSocket.close();
-                }
-            } catch (IOException e) {
-                System.out.println("IOException: " + e.getMessage());
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+
+                new Thread(() -> handleConnection(clientSocket)).start();
+                // handleConnection(clientSocket);
             }
+        } catch (IOException e) {
+            System.err.println("Server error: " + e.getMessage());
+        }
+    }
+
+    private static void handleConnection(Socket clientSocket) {
+        try (Socket socket = clientSocket) {
+            OutputStream outputStream = socket.getOutputStream();
+            BufferedReader inputStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            while (true) {
+                String firstInput = inputStream.readLine();
+                if (firstInput != null) {
+                    // Temporary additional reads to handle ping commands only
+                    inputStream.readLine();
+                    inputStream.readLine();
+                    outputStream.write("+PONG\r\n".getBytes());
+                } else {
+                    break;
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
